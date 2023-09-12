1e4b674555fd856fd77db3e23f1d888f5b8ce375
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2023-09-12 21:13:53 +0300

    Add files via upload

---
+++ 12.09.2023.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+    void sayHi() {
+        cout << "Hello, my name is " << name << ", and I am " << age << " years old." << endl;
+    }
+};
+
+bool compareStudents(const Student& s1, const Student& s2) {
+    return s1.age < s2.age;
+}
+
+int main() {
+    Student newList[3]{
+        {"Vlad", 18},
+        {"Maxim", 16},
+        {"Oleg", 20}
+    };
+
+    sort(newList, newList + 3, compareStudents);
+
+    cout << "Sorted list of students by age:" << endl;
+    for (int i = 0; i < 3; i++) {
+        newList[i].sayHi();
+    }
+
+    return 0;
+}
