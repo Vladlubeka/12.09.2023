9131fc9a9127caecc1f64c0767695673841ade0a
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2023-10-10 14:32:26 +0300

    Add files via upload

---
+++ 5.10.2023.cpp
@@ -0,0 +1,44 @@
+﻿// ConsoleApplication59.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Point {
+public:
+    int x;
+    int y;
+    int thickness = 1;
+    string color;
+
+    Point(int x = 0, int y = 0) {
+        this->x = x;
+        this->y = y;
+        this->color = "black";
+    }
+};
+
+class Line {
+public:
+    Point p1;
+    Point p2;
+    double length; 
+
+    Line(Point p1, Point p2) {
+        this->p1 = p1;
+        this->p2 = p2;
+        length = sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+    }
+};
+
+int main() {
+    Point point1(1, 2);
+    Point point2(4, 6);
+
+    Line line(point1, point2);
+
+    cout << "Length of the line: " << line.length << endl;
+
+    return 0;
+}
