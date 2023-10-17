b50e7c93fb116c8eda917de15808ad5ac9c61985
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2023-10-17 17:39:55 +0300

    Add files via upload

---
+++ 12.10.2023.cpp
@@ -0,0 +1,51 @@
+﻿// ConsoleApplication61.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+class Figure {
+public:
+    string type;
+    char symbol;
+    Figure(string type, char symbol) {
+        this->type = type;
+        this->symbol = symbol;
+    }
+};
+
+char getRandomLetter() {
+    return 'A' + rand() % 26;
+}
+
+int main() {
+    srand(static_cast<unsigned int>(time(NULL)));
+
+    Figure empty("Empty", ' ');
+
+    Figure diagram[10][10];
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            int random = rand() % 4; 
+            if (random == 0) {
+                diagram[i][j] = empty;
+            }
+            else {
+                char randomLetter = getRandomLetter();
+                diagram[i][j] = Figure("Letter", randomLetter);
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << diagram[i][j].symbol << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
