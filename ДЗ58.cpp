79acc697bd38b7b7ba8b00d4cf9d8ffd296cca0b
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2023-11-10 18:55:32 +0200

    Add files via upload

---
+++ ДЗ58.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class MorseCodeConverter {
+private:
+    static const char alphabet[];
+    static const char* morseCode[];
+
+public:
+    string textToMorse(const string& text) const {
+        string result;
+        for (char c : text) {
+            if (isupper(c)) {
+                int index = c - 'A';
+                result += morseCode[index];
+                result += ' ';
+            }
+        }
+        return result;
+    }
+
+    string morseToText(const string& morse) const {
+        string result;
+        size_t start = 0;
+        size_t end = morse.find(' ');
+
+        while (end != string::npos) {
+            string currentMorse = morse.substr(start, end - start);
+            for (int i = 0; i < 26; ++i) {
+                if (currentMorse == morseCode[i]) {
+                    result += alphabet[i];
+                    break;
+                }
+            }
+            start = end + 1;
+            end = morse.find(' ', start);
+        }
+
+        return result;
+    }
+};
+
+const char MorseCodeConverter::alphabet[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+const char* MorseCodeConverter::morseCode[] = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
