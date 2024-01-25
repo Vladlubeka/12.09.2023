e7f4a2484ee78488790d367c7cfd128beeb7fdfb
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2024-01-25 20:52:18 +0200

    Add files via upload

---
+++ 25.01.2024 номер 2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Student
+    {
+        public string FullName { get; set; }
+        public int StudentId { get; set; }
+        public int Age { get; set; }
+
+        public Student(string fullName, int studentId, int age)
+        {
+            FullName = fullName;
+            StudentId = studentId;
+            Age = age;
+        }
+    }
+
+    class StudentGroup
+    {
+        public int GroupNumber { get; set; }
+        public Student[] Students { get; set; }
+
+        public StudentGroup(int groupNumber)
+        {
+            GroupNumber = groupNumber;
+            Students = new Student[0];
+        }
+
+        public void AddStudent(Student student)
+        {
+            Array.Resize(ref Students, Students.Length + 1);
+            Students[Students.Length - 1] = student;
+        }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 25.01.2024.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[]
+     {
+            new Student { FirstName = "Iван", LastName = "Iванов", Age = 20 },
+            new Student { FirstName = "Марiя", LastName = "Петренко", Age = 19 },
+            new Student { FirstName = "Олексiй", LastName = "Сидоренко", Age = 22 },
+            new Student { FirstName = "Наталiя", LastName = "Грищенко", Age = 18 },
+     };
+
+            PrintStudentsOlderThan18(students);
+        }
+
+        static void PrintStudentsOlderThan18(Student[] students)
+        {
+            Console.WriteLine("Студенти старшi 18 рокiв:");
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i].Age > 18)
+                {
+                    Console.WriteLine("Iм'я: " + students[i].FirstName + ", Прiзвище: " + students[i].LastName + ", Вiк: " + students[i].Age + " рокiв");
+                }
+            }
+        }
+    }
+}
