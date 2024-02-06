6433f3f87c6668d336bd52a68b786a4f20d9cb77
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2024-02-06 20:53:16 +0200

    Add files via upload

---
+++ 06.02.24.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    public class Figure
+    {
+        public void CalculateArea()
+        {
+            Console.WriteLine("Площа: 0");
+        }
+    }
+
+    public class Square : Figure
+    {
+        private double sideLength;
+
+        public Square(double sideLength)
+        {
+            this.sideLength = sideLength;
+        }
+
+        public void CalculateArea()
+        {
+            Console.WriteLine("Площа квадрата: " + sideLength * sideLength);
+        }
+    }
+
+    public class Circle : Figure
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public void CalculateArea()
+        {
+            Console.WriteLine("Площа кола: " + Math.PI * radius * radius);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(5);
+            Circle circle = new Circle(3);
+
+            square.CalculateArea();
+            circle.CalculateArea();
+        }
+    }
+}
---
+++ 06.02.24впр.2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Тварина виділяє звук");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string name) : base(name)
+        {
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine(Name + " гавкає");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string name) : base(name)
+        {
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine(Name + " муркоче");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog("Барбос");
+            Cat cat = new Cat("Мурка");
+
+            dog.MakeSound(); 
+            cat.MakeSound(); 
+        }
+    }
+}
